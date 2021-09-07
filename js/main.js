6404d22c5d8024ea244838246a5e7e9e7432a547
Siddhesh Kamath <63893110+PSYCHOSIDX@users.noreply.github.com>
2021-09-07 13:36:08 +0530

    //minor changes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const button = document.querySelector('.container button');
 const jokes = document.querySelector('.container p');
+document.addEventListener('DOMContentLoaded', fetchjoke);
+button.addEventListener('click', fetchjoke);
 
-document.addEventListener('DOMContentLoaded', getjoke);
-button.addEventListener('click', getjoke);
-
-async function getjoke(){
+async function fetchjoke(){
 const jokedata = await fetch('https://icanhazdadjoke.com/',{
      headers:{
           'Accept': 'application/json'
      }
 });
-const jokeobj = await jokedata.json();
-jokes.innerHTML = jokeobj.joke;
+
+const joke_obj = await joke_data.json();
+jokes.innerHTML = joke_obj.joke;
 }
